7f06a3cd393ed205239769f56fa89ea8daea8043
suhasledange <suhasledange10@gmail.com>
2023-03-07 22:12:30 +0530

    update

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ let ar = document.querySelector(".treat");
 let dr = document.querySelector(".drop");
 li_t.addEventListener("click",()=>{
     dr.classList.toggle("show-drop");
-
+    dr.addEventListener("click",()=>{
+        dr.classList.remove("show-drop");
+    })
     if(ar.classList.contains('uil-angle-down')){
         ar.classList.remove("uil-angle-down");
         ar.classList.add("uil-angle-up")
@@ -20,9 +22,14 @@ li_t.addEventListener("click",()=>{
 })
 
 
-let li_tt = document.querySelector(".li-tt");
+let li_tt = document.querySelector(".li-more");
 let ara = document.querySelector(".more");
+let drm = document.querySelector(".drop-more1");
 li_tt.addEventListener("click",()=>{
+    drm.classList.toggle("drop-more-show");
+    drm.addEventListener("click",()=>{
+        drm.classList.remove("drop-more-show");
+    })
     if(ara.classList.contains('uil-angle-down')){
         ara.classList.remove("uil-angle-down");
         ara.classList.add("uil-angle-up")
@@ -36,19 +43,18 @@ li_tt.addEventListener("click",()=>{
 
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    //when windows width is >= 600px
+    breakpoints:{
+        600:{
+            slidesPerView:2
+        }
+    }
   });
 
 //   show hide faqs 
@@ -70,3 +76,59 @@ var swiper = new Swiper(".mySwiper", {
 
     })
   })
+
+
+  let ham_btn = document.querySelector(".hamburger");
+  let nav_c = document.querySelector(".nav-mid");
+  let mob_lan=document.querySelector(".mob-lann");
+
+  let ha1 = document.querySelector
+
+  ham_btn.addEventListener("click",()=>{
+    nav_c.classList.toggle("nav-mid-show");
+    mob_lan.classList.toggle("mob-lann-hide");
+    ham_btn.classList.toggle("ham");
+
+  });
+
+
+let animl = document.querySelectorAll('.animl');
+let animu = document.querySelectorAll('.animu');
+let animr = document.querySelectorAll('.animr');
+window.addEventListener('scroll',checkSect);
+checkSect();
+
+function checkSect(){
+    const trigger =  window.innerHeight / 5 * 4;
+
+    animl.forEach((ele) =>{
+        const eleTop = ele.getBoundingClientRect().top;
+        if(eleTop < trigger){
+            ele.classList.add("anim-show");
+
+        }
+        else{
+            ele.classList.remove('anim-show');
+        }
+    })
+    animu.forEach((ele) =>{
+        const eleTop = ele.getBoundingClientRect().top;
+        if(eleTop < trigger){
+            ele.classList.add("anim-show1");
+            
+        }
+        else{
+            ele.classList.remove('anim-show1');
+        }
+    })
+    animr.forEach((ele) =>{
+        const eleTop = ele.getBoundingClientRect().top;
+        if(eleTop < trigger){
+            ele.classList.add("anim-show2");
+            
+        }
+        else{
+            ele.classList.remove('anim-show2');
+        }
+    })
+}
